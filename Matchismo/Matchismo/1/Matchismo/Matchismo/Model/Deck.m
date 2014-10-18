c7914c97d7b3d118ea6c7e079c43c0e9d8de3231
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-10-19 00:55:23 +0530

    initial

--- Matchismo/Matchismo/1/Matchismo/Matchismo/Model/Deck.m
+++ Matchismo/Matchismo/1/Matchismo/Matchismo/Model/Deck.m
@@ -15,10 +15,20 @@
 
 @implementation Deck
 
+
+-(NSMutableArray *)cards
+{
+    if (!_cards)_cards = [[NSMutableArray alloc]init];
+    return _cards;
+}
+
 -(void)addCard:(Card *)card atTop:(BOOL)atTop
 {
-    
-    
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    }else{
+        [self.cards addObject:card];
+    }
 }
 
 -(void)addCard:(Card *)card
@@ -28,7 +38,13 @@
 
 -(Card *)drawRandomCard
 {
-    
+    Card *randomCard = nil;
+    if ([self.cards count]) {
+        unsigned index = arc4random() % [self.cards count];
+        randomCard = self.cards[index];
+        [self.cards removeObjectAtIndex:index];
+    }
+    return randomCard;
 }
 
 
