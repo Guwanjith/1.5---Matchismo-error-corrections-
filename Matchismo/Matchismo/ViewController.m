591e203e77568a5f9f21a94601cd86ca82c11ce7
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-10-19 20:26:45 +0530

    Added in flips and flipcounts

--- Matchismo/Matchismo/ViewController.m
+++ Matchismo/Matchismo/ViewController.m
@@ -10,16 +10,34 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic)int flipCount;
+
 @end
 
 @implementation ViewController
 
+-(void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    NSLog(@"flipCount has changed to %d", self.flipCount);
+}
+
 - (IBAction)touchCardButton:(UIButton *)sender
 {
-    [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+    if ([sender.currentTitle length]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                       forState:UIControlStateNormal];
-    [sender setTitle:@""
+        [sender setTitle:@""
             forState:UIControlStateNormal];
+    }else{
+        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"A♣️"
+                forState:UIControlStateNormal];
+    }
+    self.flipCount++;
 }
 
 @end
