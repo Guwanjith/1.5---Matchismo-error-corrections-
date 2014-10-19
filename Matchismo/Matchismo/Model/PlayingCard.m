0ffb68423cfecff7410a757f0fb0bfa4144fbf99
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-10-19 19:46:56 +0530

    Finished PlayingCard

--- Matchismo/Matchismo/Model/PlayingCard.m
+++ Matchismo/Matchismo/Model/PlayingCard.m
@@ -12,12 +12,11 @@
 
 -(NSString *)contents
 {
-    NSArray *rankStrings = @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+    NSArray *rankStrings = [PlayingCard rankStrings];
     return [rankStrings[self.rank] stringByAppendingString:self.suit];
 }
 
 @synthesize suit = _suit;
-
 +(NSArray *)validSuits
 {
     return @[@"♥️",@"♦️",@"♣️",@"♠️"];
@@ -29,9 +28,23 @@
         _suit = suit;
     }
 }
-
 -(NSString *)suit
 {
     return _suit ? _suit : @"?";
 }
+
++(NSArray *)rankStrings
+{
+    return @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+}
+
++(NSUInteger)maxRank{return [[self rankStrings]count]-1;}
+
+-(void)setRank:(NSUInteger)rank
+{
+    if (rank <= [PlayingCard maxRank]) {
+        _rank = rank;
+    }
+}
 @end
+
